55e1d55c5ae47deda5bc1d192b1708619ee28f30
ruz76 <jan.ruzicka.vsb@gmail.com>
2016-12-22 19:10:40 +0100

    modified:   irz/combine.sh 	modified:   irz/irz_to_csv.xsl

--- irz/combine.sh
+++ irz/combine.sh
@@ -1,12 +1,12 @@
-#wget -O irz.html "http://portal.cenia.cz/irz/unikyPrenosy.jsp?Rok=2015&UnikOvzdusi=1&Typ=bezny&Mnozstvi=*&MetodaC=1&MetodaM=1&MetodaE=1&LatkaNazev=*&Ohlasovatel=&OhlasovatelTyp=subjektNazev&EPRTR=*&NACE=*&Lokalita=cr&Adresa=&Kraj=*&CZNUTS=*&SeskupitDle=subjektu&Razeni=vzestupne&OKEC=*"
-#tidy irz.html > irz2.html
-#xsltproc --html irz_to_wget.xsl irz2.html > irz.html.sh
-#bash irz.html.sh
+wget -O irz.html "http://portal.cenia.cz/irz/unikyPrenosy.jsp?Rok=2015&UnikOvzdusi=1&Typ=bezny&Mnozstvi=*&MetodaC=1&MetodaM=1&MetodaE=1&LatkaNazev=*&Ohlasovatel=&OhlasovatelTyp=subjektNazev&EPRTR=*&NACE=*&Lokalita=cr&Adresa=&Kraj=*&CZNUTS=*&SeskupitDle=subjektu&Razeni=vzestupne&OKEC=*"
+tidy irz.html > irz2.html
+xsltproc --html irz_to_wget.xsl irz2.html > irz.html.sh
+bash irz.html.sh
 xsltproc --html irz_to_csv.xsl irz.html > irz.csv
 tail -n +2 irz.csv > pom
 cat pom | tr -d '"' | tr -d "'" > irz.csv
 rm pom
-echo "NAME;ID;X;Y;LATKA;LATKAID;MNOZSTVI;TYPURCENI";
+echo "NAME;ID;X;Y;POLLUTANT;POLLUTANTID;AMOUNT;TYPEOFDETERMINATION";
 while read line           
 do           
     NAME=`echo $line | cut -d";" -f1`;
@@ -18,14 +18,12 @@ do
     for (( a=3; a<=$ct; a+=2 ))
     do  
         let "b = $a + 1"
-        LATKA=`echo $line | cut -d";" -f$a`;
-        LATKA1=`echo $LATKA | cut -d"(" -f1 | xargs`;
-        LATKA2=`echo $LATKA | cut -d"(" -f2 | cut -d")" -f1`;
-        MNOZSTVI=`echo $line | cut -d";" -f$b`;
-        MNOZSTVI1=`echo $MNOZSTVI | cut -d"[" -f1 | xargs`;
-        MNOZSTVI2=`echo $MNOZSTVI | cut -d"[" -f2 | cut -d"]" -f1`;
-        #echo $a;
-        #echo $b;
-        echo "\""$NAME"\";"$ID";-"$X";-"$Y";"$LATKA1";"$LATKA2";"$MNOZSTVI1";"$MNOZSTVI2;           
+        POLLUTANT=`echo $line | cut -d";" -f$a`;
+        POLLUTANT1=`echo $POLLUTANT | cut -d"(" -f1 | xargs`;
+        POLLUTANT2=`echo $POLLUTANT | cut -d"(" -f2 | cut -d")" -f1`;
+        AMOUNT=`echo $line | cut -d";" -f$b`;
+        AMOUNT1=`echo $AMOUNT | cut -d"[" -f1 | xargs`;
+        AMOUNT2=`echo $AMOUNT | cut -d"[" -f2 | cut -d"]" -f1`;
+        echo "\""$NAME"\";"$ID";-"$X";-"$Y";"$POLLUTANT1";"$POLLUTANT2";"$AMOUNT1";"$AMOUNT2;           
     done
 done <irz.csv 
--- irz/irz_to_csv.xsl
+++ irz/irz_to_csv.xsl
@@ -4,21 +4,8 @@
 <xsl:output method="text"/>
 <xsl:template match="/">
    <xsl:for-each select="//table[@class='list']/tr">
-    <!--<xsl:if test="td[1] != ''">  
-        <xsl:text>&#xa;</xsl:text>
-        <xsl:value-of select="td[1]"/><xsl:text>;</xsl:text>	
-    </xsl:if>
-    -->
     <xsl:if test="td[2]/a/@href != ''">  
         <xsl:text>&#xa;</xsl:text>
-        <!--<xsl:variable name="newname">
-        <xsl:call-template name="string-replace-all">
-            <xsl:with-param name="text" select="td[2]" />
-            <xsl:with-param name="replace" select=";" />
-            <xsl:with-param name="by" select=" " />
-        </xsl:call-template>
-        </xsl:variable>
-        -->
         <xsl:value-of select="translate(td[2],';',' ')"/><xsl:text>;</xsl:text>	
         <xsl:value-of select="substring-after(td[2]/a/@href, 'id=')"/><xsl:text>;</xsl:text>	
     </xsl:if>
@@ -30,30 +17,6 @@
     </xsl:if>    
    </xsl:for-each>
    <xsl:text>&#xa;</xsl:text>
-</xsl:template>
-
-<xsl:template name="string-replace-all">
-    <xsl:param name="text" />
-    <xsl:param name="replace" />
-    <xsl:param name="by" />
-    <xsl:choose>
-        <xsl:when test="$text = '' or $replace = ''or not($replace)" >
-            <!-- Prevent this routine from hanging -->
-            <xsl:value-of select="$text" />
-        </xsl:when>
-        <xsl:when test="contains($text, $replace)">
-            <xsl:value-of select="substring-before($text,$replace)" />
-            <xsl:value-of select="$by" />
-            <xsl:call-template name="string-replace-all">
-                <xsl:with-param name="text" select="substring-after($text,$replace)" />
-                <xsl:with-param name="replace" select="$replace" />
-                <xsl:with-param name="by" select="$by" />
-            </xsl:call-template>
-        </xsl:when>
-        <xsl:otherwise>
-            <xsl:value-of select="$text" />
-        </xsl:otherwise>
-    </xsl:choose>
 </xsl:template>
 
 </xsl:stylesheet>
